6e35cf05bdbb42015153661c5d9557674e991a9a
InvalidSB <invalidSB45@gmail.com>
2021-06-30 13:04:13 +0545

    initial setup

---
+++ src/Colors.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+toast.configure()
+
+function Colors() {
+    const [ColorNames]=useState(['#12CBC4','#0652DD','#FFC312',"#FF0066",'#5758BB','#ff4757','#0abde3','#5f27cd','#00d2d3','#576574','#f368e0','#48dbfb','#222f3e','#4b4b4b','#1e90ff',"green","black","yellow","blue","cyan"])
+    const colorCopy=(e)=>{
+        let clickedColor= e.target.innerText;
+        if (navigator.clipboard){
+            navigator.clipboard.writeText(clickedColor)
+            // navigator.clipboard.readText()
+            toast.success(`${clickedColor} Copied to Clipboard`,{
+                position:toast.POSITION.BOTTOM_CENTER
+            })
+        }
+
+    }
+    return (
+        <div className="row">
+            {ColorNames.map((color)=>{return(
+                <div className="each">
+                <div className="innereach" key={color} style={{background:color}} onClick={colorCopy}>
+                    <div>
+                        {color}
+                    </div>
+                </div>
+                </div>
+            )})}
+        </div>
+    )
+}
+
+export default Colors
