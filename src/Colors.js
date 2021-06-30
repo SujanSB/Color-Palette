734ada90e2f5cb19fe47ad82ba675474c5643d0b
InvalidSB <invalidSB45@gmail.com>
2021-06-30 13:13:29 +0545

    initial setup

--- src/Colors.js
+++ src/Colors.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 toast.configure()
